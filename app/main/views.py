9ab5bc6c86a4f8e0e62565f101331a62fe2d49cf
alex <alexkimutai021@gmail.com>
2020-07-22 22:46:26 +0300

    profile

--- app/main/views.py
+++ app/main/views.py
@@ -2,3 +2,11 @@ from flask import render_template,request,redirect,url_for
 from . import main
 from flask_login import login_required
 
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
