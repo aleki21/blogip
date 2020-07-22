10a1f301f08b6564a722551b509459a4955ebce3
alex <alexkimutai021@gmail.com>
2020-07-22 22:57:40 +0300

    update profile photo

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ class Config:
     '''
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://alex:password@localhost/blog'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
 
 
 
